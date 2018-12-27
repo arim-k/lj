20bbe7e0370d75f2c2c9286e46e169be552e0f1a
splashwave <arim721@gmail.com>
2018-12-28 01:24:35 +0900

    part7

--- public/es6/part7/scope.js
+++ public/es6/part7/scope.js
@@ -1,6 +1,40 @@
 // 스코프는 변수,상수,매개변수 언제 어디서 정의되는지 결정
 function f(x) {
-    return x + 3;
+  return x + 3;
 }
 f(5);
 x;
+
+// js의 스코프는 정적
+function f1() {
+  console.log("one");
+}
+function f2() {
+  console.log("two");
+}
+f2();
+f1();
+f2();
+
+//
+const x = 3;
+
+function f() {
+  console.log(x);
+  console.log(y);
+}
+{
+  // 새 스코프
+  const y = 5;
+  f();
+}
+
+// 전역 스코프
+let name = "Irena";
+let age = 25;
+function great() {
+  console.log(`Hello ${name}`);
+}
+function getBirthYear() {
+  return new Date().getFullYear() - age;
+}
